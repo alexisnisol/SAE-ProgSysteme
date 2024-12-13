fb7a95cb38065472db83e7b70f70b7c11b5e1e97
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-12-13 17:01:03 +0100

    Update du Client (Scanner)

--- src/network/Client.java
+++ src/network/Client.java
@@ -5,6 +5,7 @@ import network.utils.Network;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -13,8 +14,18 @@ public class Client {
     public Client() {
         try {
             this.socket = new Socket(Constant.SERVER_HOST, Constant.PORT);
-            Network.sendMessage("connect Player"+(int)(Math.random()*100), socket);
-            System.out.println(Network.getMessage(socket));
+
+            Scanner scanner = new Scanner(System.in);
+            boolean quit = false;
+            String message;
+            while ((message = scanner.nextLine()) != null && !quit) {
+                Network.send(message, socket);
+                String serverResponse = Network.receive(socket);
+                System.out.println(serverResponse);
+                if(message.equalsIgnoreCase("QUIT") && serverResponse.equals(Constant.STATUS_OK)) {
+                    quit = true;
+                }
+            }
 
             socket.close();
         } catch (IOException e) {
