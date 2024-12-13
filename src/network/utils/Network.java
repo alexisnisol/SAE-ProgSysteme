0e2a708420cdc29460c56e055d83be9caef72976
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-12-13 13:21:58 +0100

    Ajout de classes utils (Constant et Network) pour les sockets

---
+++ src/network/utils/Network.java
@@ -0,0 +1,35 @@
+package network.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Network {
+
+    private Network() {
+    }
+
+    public static String getMessage(Socket socket) {
+        String message = "";
+        try {
+            InputStreamReader stream = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(stream);
+            message = reader.readLine();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+    public static void sendMessage(String message, Socket socket) {
+        try {
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println(message);
+            writer.flush();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
