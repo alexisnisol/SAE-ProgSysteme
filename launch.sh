c571b60662fb0c4fb36c4e43a94c27f272f5eb0e
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2024-12-16 09:56:48 +0100

    Script lancement

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 # Compilation des sources principales
-javac --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "lib/*" -d bin src/modele/*.java src/modele/controller/*.java src/modele/exception/*.java src/modele/**/*.java src/vue/*.java
+javac --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "lib/*" -d bin src/model/*.java src/controller/*.java src/model/exception/*.java src/model/**/*.java src/view/*.java
 
 # Exécution de la classe ApplicationJeuxOlympique
-java -cp bin modele.Main
+java -cp bin model.Main
 
-java --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "bin:/usr/share/java/mariadb-java-client.jar:lib/*:src/ressources" vue.Appli #2>/dev/null
+java --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "bin:/usr/share/java/mariadb-java-client.jar:lib/*:src/ressources" view.Appli #2>/dev/null
 
