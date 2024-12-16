a45b3660ac6d215645215e3aa87df38188bbaa40
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2024-12-16 09:23:46 +0100

    Finalisation du puissance 4 sur Terminal

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 javac --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "lib/*" -d bin src/modele/*.java src/modele/controller/*.java src/modele/exception/*.java src/modele/**/*.java src/vue/*.java
 
 # Exécution de la classe ApplicationJeuxOlympique
-# java --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "bin:/usr/share/java/mariadb-java-client.jar:lib/*:src/ressources" src.vues.ApplicationJeuxOlympique
+java -cp bin modele.Main
+
 java --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp "bin:/usr/share/java/mariadb-java-client.jar:lib/*:src/ressources" vue.Appli #2>/dev/null
 
