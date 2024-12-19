33a8be565f5b4a1ce581aba8c98a1cdc49e329bd
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-12-19 20:31:32 +0100

    Update Client (Threads saisie clavier, écoute socket) + Gestion exception en cas d'arrêt du socket

--- src/network/Client.java
+++ src/network/Client.java
@@ -10,25 +10,49 @@ import java.util.Scanner;
 public class Client {
 
     private Socket socket;
+    private boolean quit;
 
     public Client() {
+        quit = false;
         try {
             this.socket = new Socket(Constant.SERVER_HOST, Constant.PORT);
 
-            Scanner scanner = new Scanner(System.in);
-            boolean quit = false;
-            String message;
-            while ((message = scanner.nextLine()) != null && !quit) {
-                Network.send(message, socket);
-                String serverResponse = Network.receive(socket);
-                System.out.println(serverResponse);
-                if(message.equalsIgnoreCase("QUIT") && serverResponse.equals(Constant.STATUS_OK)) {
+            Thread sendThread = new Thread(() -> {
+                Scanner scanner = new Scanner(System.in);
+                String message;
+                try {
+                    while ((message = scanner.nextLine()) != null && !quit) {
+                        Network.send(message, socket);
+                        if (message.equalsIgnoreCase("QUIT")) {
+                            quit = true;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                     quit = true;
                 }
-            }
+            });
+
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    while(!quit) {
+                        String serverResponse = Network.receive(socket);
+                        System.out.println(serverResponse);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Server disconnected");
+                    quit = true;
+                }
+            });
+
+            sendThread.start();
+            receiveThread.start();
+
+            sendThread.join();
+            receiveThread.join();
 
             socket.close();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
