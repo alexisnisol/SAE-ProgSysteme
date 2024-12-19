b5e146bd389034df1fb81e726e57875635ecaaa0
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-12-19 20:29:36 +0100

    Update du Player (en jeu, invitation...)

--- src/network/Player.java
+++ src/network/Player.java
@@ -3,9 +3,22 @@ package network;
 public class Player {
 
     private String name;
+    private boolean inGame;
+    private String pendingRequest;
+    private String currentGame;
+    private ClientHandler clientHandler;
 
     public Player(String name) {
         this.name = name;
+        this.inGame = false;
+    }
+
+    public void setClientHandler(ClientHandler clientHandler) {
+        this.clientHandler = clientHandler;
+    }
+
+    public ClientHandler getClientHandler() {
+        return this.clientHandler;
     }
 
     public String getName() {
@@ -14,6 +27,29 @@ public class Player {
 
     public static boolean isValidPlayerName(String playerName) {
         return playerName != null && playerName.matches("[a-zA-Z0-9]{3,10}");
+    }
+
+    public boolean isAvailable() {
+        return !this.inGame && this.pendingRequest == null;
+    }
+
+    public void setRequest(Player playerSource) {
+        this.pendingRequest = playerSource.getName();
+    }
+
+    public String getRequest() {
+        return this.pendingRequest;
+    }
+
+    public void clearRequest() {
+        this.pendingRequest = null;
+    }
+
+    public void setInGame() {
+        if(this.isAvailable()) {
+            this.inGame = true;
+            this.currentGame = "";
+        }
     }
 
     @Override
