8775d43caacdb9b2b22a61edaabc69e4fd2cbf77
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-12-19 20:30:46 +0100

    Gestion des exceptions avec les échanges socket

--- src/network/utils/Network.java
+++ src/network/utils/Network.java
@@ -1,6 +1,7 @@
 package network.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -10,26 +11,15 @@ public class Network {
     private Network() {
     }
 
-    public static String receive(Socket socket) {
-        String message = "";
-        try {
-            InputStreamReader stream = new InputStreamReader(socket.getInputStream());
-            BufferedReader reader = new BufferedReader(stream);
-            message = reader.readLine();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return message;
+    public static String receive(Socket socket) throws IOException {
+        InputStreamReader stream = new InputStreamReader(socket.getInputStream());
+        BufferedReader reader = new BufferedReader(stream);
+        return reader.readLine();
     }
 
-    public static void send(String message, Socket socket) {
-        try {
-            PrintWriter writer = new PrintWriter(socket.getOutputStream());
-            writer.println(message);
-            writer.flush();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void send(String message, Socket socket) throws IOException {
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        writer.println(message);
+        writer.flush();
     }
 }
