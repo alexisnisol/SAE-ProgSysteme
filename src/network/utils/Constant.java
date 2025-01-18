30b94a9c5dc03412851620443d20995515312481
Alexis Nisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-18 19:07:44 +0100

    Fix constant

--- src/network/utils/Constant.java
+++ src/network/utils/Constant.java
@@ -4,10 +4,10 @@ public class Constant {
     public static final int PORT = 4444;
     public static final String SERVER_HOST = "127.0.0.1";
 
-    public static final String HOST = "servinfo-maria";
-    public static final String DB_NAME = "DBnagarajah";
-    public static final String USER = "nagarajah";
-    public static final String PASSWORD = "nagarajah";
+    public static final String HOST = "localhost";
+    public static final String DB_NAME = "saeprog";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
 
     public static final String STATUS_OK = "OK";
     public static final String STATUS_ERR = "ERR";
