0e1ad9cab213db163f48f3de5079c05414cee823
Alexis Nisol <alexis.nisol@etu.univ-orleans.fr>
2025-01-18 16:25:07 +0100

    Update Constant.java

--- src/network/utils/Constant.java
+++ src/network/utils/Constant.java
@@ -2,7 +2,12 @@ package network.utils;
 
 public class Constant {
     public static final int PORT = 4444;
-    public static final String SERVER_HOST = "127.0.0.1";//"192.168.28.197";
+    public static final String SERVER_HOST = "127.0.0.1";
+
+    public static final String HOST = "servinfo-maria";
+    public static final String DB_NAME = "DBnagarajah";
+    public static final String USER = "nagarajah";
+    public static final String PASSWORD = "nagarajah";
 
     public static final String STATUS_OK = "OK";
     public static final String STATUS_ERR = "ERR";
